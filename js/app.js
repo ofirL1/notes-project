cd866a52eb39edb969cd4c0e2e14f7c188d42f94
Ofir <ofirluzon68@gmail.com>
2020-11-12 20:06:01 +0200

    fix validation removed else

--- js/app.js
+++ js/app.js
@@ -6,26 +6,20 @@ let pError = document.querySelector(".error");
 let saveNote = document.querySelector("#save");
 
 
-// vaild funcition check that all information insert
 saveNote.addEventListener('click', validationForm);
 
+// vaild funcition check that all information insert. if no give error message. else create note 
 function validationForm() {
     let str = "";
 
     if (!messageEl.value) {
         str += "Must enter message to note <br>"
-    } else {
-        str.replace("Must enter message to note <br>", "")
     }
     if (!dateEl.value) {
         str += "Must enter date to note <br>"
-    } else {
-        str.replace("Must enter date to note <br>", "")
     }
     if (!timeEl.value) {
         str += "Must enter time to note <br>"
-    } else {
-        str.replace("Must enter time to note <br>", "")
     }
     pError.style.color = "#ff0000"
     pError.innerHTML = str;
