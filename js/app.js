83be17556d72e49ff829713af8957119b3b535d3
Ofir <ofirluzon68@gmail.com>
2020-11-20 15:18:54 +0200

    final project

--- js/app.js
+++ js/app.js
@@ -17,11 +17,15 @@ console.log(d);
 // let str = d.getFullYear-d.getMonth-d.getDay
 dateEl.setAttribute("min", `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`);
 
+//variable for count id;
+let id = 0;
+
 //create object note that have this keys
 let note = {
     text: "",
     date: "",
-    time: ""
+    time: "",
+    id: null
 }
 
 //array to store all note we create
@@ -32,7 +36,7 @@ checkLocalStorage();
 
 //check if we have in localStorage data of notes, if yes we display them on screen with createNote function
 function checkLocalStorage() {
-    let arr = JSON.parse(localStorage.getItem("notes"));;
+    let arr = JSON.parse(localStorage.getItem("notes"));
     if (arr) {
         console.log(arr);
         arr.forEach(element => {
@@ -64,20 +68,21 @@ function validationForm() {
 
 
 
-
 //create note , insert new note to the container, create elments with classes we build before
-function createNote(message, date, time) {
+function createNote(text, date, time) {
 
     //insert note object the values for keys
     note = {
-        text: message,
+        text: text,
         date: date,
-        time: time
+        time: time,
+        id: id
     }
+
+    id++;
     //insert to array note object 
     noteArray.push(note);
     console.log(noteArray);
-
     //create elements for note, note = div with 4 childs (textArea,removeBtn that have icon ,p width date info + p with time info)
     let div = document.createElement('div');
     let textArea = document.createElement('textarea');
@@ -107,6 +112,8 @@ function createNote(message, date, time) {
 
     //add listner to removeButton.
     removeBtn.addEventListener('click', removeNote);
+    //send to function updateNote the textArea element to bo this , and the note  id of the current note clicked.
+    textArea.addEventListener('change',updateNote.bind(textArea,note.id));
     //insert to localStorage the noteArray with objects of note
     localStorage.setItem("notes", JSON.stringify(noteArray));
     console.log(localStorage.getItem("notes"));
@@ -114,6 +121,15 @@ function createNote(message, date, time) {
 
 };
 
+function updateNote(noteId){
+    let text = this.value;
+    noteArray.forEach(function(element){
+        if(element.id === noteId){
+            element.text = text;
+        }
+    });
+    localStorage.setItem("notes", JSON.stringify(noteArray));
+};
 
 //remove note from cilent display + localStorage
 function removeNote() {
@@ -143,10 +159,8 @@ function clearNotes() {
     if (confirm === true) {
         for (let i = 0; i < notesEl.length; i++) {
             notesEl[i].remove();
-            console.log(i)
-
         }
-        localStorage.setItem("notes", null);
+        localStorage.clear;
         noteArray = [];
     }
 }
